2cc5c4745791e333102176c4b76cd9086e8c9ef6
Amal <amol43@outlook.com>
2023-09-04 14:42:36 +0300

    update

--- MusouqSystem/src/main/java/com/example/musouqsystem/MusouqSystemApplication.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/MusouqSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MusouqSystemApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(MusouqSystemApplication.class, args);
 	}
