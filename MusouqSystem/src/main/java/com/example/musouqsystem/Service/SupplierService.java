f81846ff497959047d9a9d63e59f271c20f05eca
Amal <amol43@outlook.com>
2023-09-06 09:23:58 +0300

    supplier repo & service

---
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Service/SupplierService.java
@@ -0,0 +1,14 @@
+package com.example.musouqsystem.Service;
+
+import com.example.musouqsystem.Repository.SupplierRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class SupplierService {
+    private final SupplierRepository supplierRepository;
+
+
+
+}
