a43ebe9e63d3bce544b4992b25d485c34b37d114
Amal <amol43@outlook.com>
2023-09-06 13:07:29 +0300

    Category CRUD

--- MusouqSystem/src/main/java/com/example/musouqsystem/Service/CategoryService.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Service/CategoryService.java
@@ -8,12 +8,39 @@ import com.example.musouqsystem.Repository.SupplierRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class CategoryService {
     private final CategoryRepository categoryRepository;
     private final SupplierRepository supplierRepository;
 
+
+    public List<Category> supplierGetAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+//    admin auth
+    public void addCategory(Category category) {
+        categoryRepository.save(category);
+    }
+
+
+    public void updateCategory(Integer category_id, Category category) {
+        Category oldCategory = categoryRepository.findCategoryById(category_id);
+        if (oldCategory == null) throw new ApiException("category not exist");
+
+        oldCategory.setTitle(category.getTitle());
+        categoryRepository.save(oldCategory);
+    }
+
+    public void deleteCategory(Integer category_id) {
+        Category oldCategory = categoryRepository.findCategoryById(category_id);
+        if (oldCategory == null) throw new ApiException("category not exist");
+
+        categoryRepository.delete(oldCategory);
+    }
 
 
     public void assignSupplierToCategory(Integer supplier_id, Integer category_id) {
