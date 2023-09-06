cca1637d6d1e946a9566aa9480753b9d986ad600
Amal <amol43@outlook.com>
2023-09-06 15:04:42 +0300

    add product endpoints

--- MusouqSystem/src/main/java/com/example/musouqsystem/Repository/ProductRepository.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Repository/ProductRepository.java
@@ -4,7 +4,13 @@ import com.example.musouqsystem.Model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
     Product findProductById(Integer id);
+
+    List<Product> findProductsBySupplierId(Integer supplier_id);
+
+//    List<Product> findProductsByMarketerId(Integer marketer_id);
 }
--- MusouqSystem/src/main/java/com/example/musouqsystem/Service/ProductService.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Service/ProductService.java
@@ -1,26 +1,65 @@
 package com.example.musouqsystem.Service;
 
 import com.example.musouqsystem.Api.ApiException;
-import com.example.musouqsystem.Model.Product;
-import com.example.musouqsystem.Model.Supplier;
-import com.example.musouqsystem.Repository.ProductRepository;
-import com.example.musouqsystem.Repository.SupplierRepository;
+import com.example.musouqsystem.Model.*;
+import com.example.musouqsystem.Repository.*;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ProductService {
     private final ProductRepository productRepository;
     private final SupplierRepository supplierRepository;
+    private final ShopperRepository shopperRepository;
+    private final OrdersRepository ordersRepository;
+    private final MarketerRepository marketerRepository;
 
-//    shopper add product to order
-//    supplier add product
+
+//    marketer & supplier
+    public List<Product> getAllProductsOfSupplier(Integer supplier_id) {
+        return productRepository.findProductsBySupplierId(supplier_id);
+    }
+
+//    shopper
+//    public List<Product> getAllProductsOfMarketer(Integer marketer_id) {
+//        return productRepository.findProductsByMarketerId(marketer_id);
+//    }
+
 //    marketer add product - path variable
 
-    public void shopperAddProductToOrder(Integer shopper_id) {
+    public void supplierAddProduct(Integer supplier_id, Product product) {
+        Supplier supplier = supplierRepository.findSupplierById(supplier_id);
+
+        if (supplier == null) throw new ApiException("supplier not exist");
+
+        product.setSupplier(supplier);
+        productRepository.save(product);
+    }
+
+    public void marketerAddProduct(Integer marketer_id, Integer product_id, Integer supplier_id ) {
+        Marketer marketer = marketerRepository.findMarketerById(marketer_id);
+        Product product = productRepository.findProductById(product_id);
+        Supplier supplier = supplierRepository.findSupplierById(supplier_id);
+
+        if (marketer == null || product == null || supplier == null) throw new ApiException("cannot add product to marketer");
 
     }
+
+    public void shopperAddProductToOrder(Integer shopper_id,Integer product_id, Integer order_id) {
+        Shopper shopper = shopperRepository.findShopperById(shopper_id);
+        Product product = productRepository.findProductById(product_id);
+        Orders order = ordersRepository.findOrdersById(order_id);
+
+        if (shopper == null || product == null || order == null) throw new ApiException("cannot add product to order");
+
+        product.setOrders(order);
+        productRepository.save(product);
+    }
+
+
 
     public void assignSupplierToProduct(Integer supplier_id, Integer product_id){
         Supplier supplier = supplierRepository.findSupplierById(supplier_id);
