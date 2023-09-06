eb8a26a1e067e16fdbb573fafcf89414c090049b
Amal <amol43@outlook.com>
2023-09-06 10:18:55 +0300

    supplier CRUD

---
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Controller/SupplierController.java
@@ -0,0 +1,39 @@
+package com.example.musouqsystem.Controller;
+
+import com.example.musouqsystem.Api.ApiResponse;
+import com.example.musouqsystem.DTO.SupplierDTO;
+import com.example.musouqsystem.Service.SupplierService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/supplier")
+public class SupplierController {
+    private final SupplierService supplierService;
+
+    @GetMapping("/getAllSupplier")
+    public ResponseEntity marketerGetAllSuppliers() {
+        return ResponseEntity.status(200).body(supplierService.marketerGetAllSuppliers());
+    }
+
+    @PostMapping("/completeProfile")
+    public ResponseEntity completeProfile(@RequestBody @Valid SupplierDTO supplierDTO){
+        supplierService.completeProfile(supplierDTO);
+        return ResponseEntity.status(200).body(new ApiResponse("your profile completed"));
+    }
+
+    @PutMapping("/updateProfile/{supplier_id}")
+    public ResponseEntity updateProfile(@PathVariable Integer supplier_id, @RequestBody @Valid SupplierDTO supplierDTO) {
+        supplierService.updateProfile(supplier_id, supplierDTO);
+        return ResponseEntity.status(200).body(new ApiResponse("your profile updated successfully"));
+    }
+
+    @DeleteMapping("/deleteAccount/{supplier_id}")
+    public ResponseEntity deleteAccount(@PathVariable Integer supplier_id) {
+        supplierService.deleteAccount(supplier_id);
+        return ResponseEntity.status(200).body(new ApiResponse("your account deleted successfully"));
+    }
+}
--- MusouqSystem/src/main/java/com/example/musouqsystem/Service/SupplierService.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Service/SupplierService.java
@@ -1,13 +1,47 @@
 package com.example.musouqsystem.Service;
 
+import com.example.musouqsystem.Api.ApiException;
+import com.example.musouqsystem.DTO.SupplierDTO;
+import com.example.musouqsystem.Model.Supplier;
 import com.example.musouqsystem.Repository.SupplierRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class SupplierService {
     private final SupplierRepository supplierRepository;
+
+
+    public List<Supplier> marketerGetAllSuppliers() {
+        return supplierRepository.findAll();
+    }
+
+    public void completeProfile(SupplierDTO supplierDTO) {
+        Supplier supplier = new Supplier(null, supplierDTO.getName(), supplierDTO.getPhone(),null,null,null);
+        supplierRepository.save(supplier);
+    }
+
+    public void updateProfile(Integer supplier_id, SupplierDTO supplierDTO) {
+        Supplier supplier = supplierRepository.findSupplierById(supplier_id);
+
+        if (supplier == null) throw new ApiException("supplier not exist");
+
+        supplier.setName(supplierDTO.getName());
+        supplier.setPhone(supplierDTO.getPhone());
+    }
+
+
+    // TODO: 9/6/2023   add conditional on delete function about order status & req status.
+    public void deleteAccount(Integer supplier_id) {
+        Supplier supplier = supplierRepository.findSupplierById(supplier_id);
+
+        if (supplier == null) throw new ApiException("supplier not exist");
+
+        supplierRepository.delete(supplier);
+    }
 
 
 
