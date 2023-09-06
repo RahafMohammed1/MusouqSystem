dca07713fc1d95d1d55e04d28f519f381c27ceff
RahafMohammed1 <111027035+RahafMohammed1@users.noreply.github.com>
2023-09-06 11:37:38 +0300

    MarketerDTO,MarkterRepository.Marketer

---
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Repository/MarketerRepository.java
@@ -0,0 +1,14 @@
+package com.example.musouqsystem.Repository;
+
+import com.example.musouqsystem.Model.Marketer;
+import com.example.musouqsystem.Model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MarketerRepository extends JpaRepository<Marketer, Integer> {
+    Marketer findMarketerById(Integer marketer_id);
+
+}
