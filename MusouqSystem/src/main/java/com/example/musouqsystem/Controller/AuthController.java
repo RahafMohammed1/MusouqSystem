1a7162d8024ef83cd833b200d3a689d3e5711d2b
Amal <amol43@outlook.com>
2023-09-07 14:54:51 +0300

    auth register

--- MusouqSystem/src/main/java/com/example/musouqsystem/Controller/AuthController.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Controller/AuthController.java
@@ -1,8 +1,14 @@
 package com.example.musouqsystem.Controller;
 
+import com.example.musouqsystem.Api.ApiResponse;
+import com.example.musouqsystem.Model.User;
 import com.example.musouqsystem.Service.AuthService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,4 +17,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/auth")
 public class AuthController {
     private final AuthService authService;
+
+//    @PostMapping("/adminRegister")
+//    public ResponseEntity adminRegister(@RequestBody @Valid User user) {
+//        authService.adminRegister(user);
+//        return ResponseEntity.status(200).body(new ApiResponse("success registration for admin.."));
+//    }
+//
+//    @PostMapping("/supplierRegister")
+//    public ResponseEntity supplierRegister(@RequestBody @Valid User user) {
+//        authService.supplierRegister(user);
+//        return ResponseEntity.status(200).body(new ApiResponse("success registration for supplier.."));
+//    }
+//
+//    @PostMapping("/shopperRegister")
+//    public ResponseEntity shopperRegister(@RequestBody @Valid User user) {
+//        authService.shopperRegister(user);
+//        return ResponseEntity.status(200).body(new ApiResponse("success registration for shopper.."));
+//    }
+//
+//    @PostMapping("/marketerRegister")
+//    public ResponseEntity marketerRegister(@RequestBody @Valid User user) {
+//        authService.marketerRegister(user);
+//        return ResponseEntity.status(200).body(new ApiResponse("success registration for marketer.."));
+//    }
+
 }
--- MusouqSystem/src/main/java/com/example/musouqsystem/Service/AuthService.java
+++ MusouqSystem/src/main/java/com/example/musouqsystem/Service/AuthService.java
@@ -17,4 +17,34 @@ public class AuthService {
         return authRepository.findAll();
     }
 
+//
+//    public void adminRegister(User user) {
+//        String hash = new BCryptPasswordEncoder().encode(user.getPassword());
+//        user.setPassword(hash);
+//        user.setRole("ADMIN");
+//        authRepository.save(user);
+//    }
+//
+//    public void supplierRegister(User user) {
+//        String hash = new BCryptPasswordEncoder().encode(user.getPassword());
+//        user.setPassword(hash);
+//        user.setRole("SUPPLIER");
+//        authRepository.save(user);
+//    }
+//
+//
+//    public void shopperRegister(User user) {
+//        String hash = new BCryptPasswordEncoder().encode(user.getPassword());
+//        user.setPassword(hash);
+//        user.setRole("SHOPPER");
+//        authRepository.save(user);
+//    }
+//
+//    public void marketerRegister(User user) {
+//        String hash = new BCryptPasswordEncoder().encode(user.getPassword());
+//        user.setPassword(hash);
+//        user.setRole("MARKETER");
+//        authRepository.save(user);
+//    }
+
 }
